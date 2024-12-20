a1e410d3f4a985f2e8d8d8a32238b97e0ac7e286
Priya121404 <priyachauhan121404@gmail.com>
2024-12-20 17:24:54 +0530

    Operators And Conditions

---
+++ OperatorsAndConditions.js
@@ -0,0 +1,76 @@
+//single line comment
+
+//Airthmetic operations 
+//1. +
+//2. -
+//3. *
+//4. /
+//5. %
+//6. ** exponent
+
+//Urinary Operator
+//Increment a++   //postIncrement and preIncrement
+//Decrement a--
+
+//Assignmenet operators
+//1. =
+//2. +=
+//3. *=
+//4. -=
+//6. **=
+
+//Comparison operators
+// 1. ==
+//2. !=
+//3. >, >=
+//4 < , <=
+
+//if there is a single number in a string form then interally 
+// first javascript convert that into number and then work on that
+
+//if we doesnt want the uper situtation to occue we use the strict version 
+
+//1. ===
+//2. !==
+
+//Logical operators
+//and &&
+// or || 
+// logical not !
+
+
+
+//Conditional statement
+
+let color;
+
+if(mode === 'dark-mode')
+{
+    color = 'black';       //if one statement then we can skip the {}
+}
+
+//if
+//if-else Statements
+// else if Statements
+
+//Ternary Operators
+
+age > 18 ? "adult" : "notadult";   //compact if else
+
+//Switch statements
+
+// switch (expression) {
+//     case caseExpression1:
+//       statements
+//     case caseExpression2:
+//       statements
+//     // …
+//     case caseExpressionN:
+//       statements
+//     default:
+//       statements
+//   }
+
+alert("error"); //its an pop up that occur on the page
+
+let name = prompt("Enter the name ");  //It is also an pop but it also takes the input
